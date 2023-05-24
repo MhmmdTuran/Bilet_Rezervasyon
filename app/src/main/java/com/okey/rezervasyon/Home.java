be63e81240eb077f668860a8de714584fc9b8e0b
Muhammed Osman TURAN <muhammed43turan@gmail.com>
2023-05-24 18:48:36 +0300

    home screen

--- app/src/main/java/com/okey/rezervasyon/Home.java
+++ app/src/main/java/com/okey/rezervasyon/Home.java
@@ -9,6 +9,9 @@ import android.widget.Button;
 
 public class Home extends AppCompatActivity {
     Button cikis;
+    Button bus;
+    Button hotel;
+    Button userInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +27,39 @@ public class Home extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        bus = findViewById(R.id.btnBus);
+
+        bus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),BusReservation.class);
+                startActivity(intent);
+
+            }
+        });
+
+        hotel = findViewById(R.id.btnHotel);
+
+        hotel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),HotelReservation.class);
+                startActivity(intent);
+            }
+        });
+
+
+        userInfo = findViewById(R.id.btnUserInfo);
+
+        userInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),UserInformations.class);
+                startActivity(intent);
+            }
+        });
+
 
 
 
---
+++ app/src/main/java/com/okey/rezervasyon/HotelReservation.java
@@ -0,0 +1,14 @@
+package com.okey.rezervasyon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HotelReservation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hotel_reservation);
+    }
+}
